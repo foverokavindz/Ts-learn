0a80543dab2533c77dba0bf6b5c9d37229dc3d59
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-02-24 10:32:20 +0530

    first Commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log('Hellow world');
+
+let hi: string = 'Kavinda';
+
+// ----------------- Data types
+let age: number = 25;
+let studentName: string = 'Saman Kumara';
+let isActive: boolean = true;
+
+// null and undefined
+let myOject: null = null;
+let undefinedData: undefined = undefined;
+
+// Arrays
+let animals: Array<string> = ['', '', ''];
+let vehicals: string[] = ['', '', ''];
